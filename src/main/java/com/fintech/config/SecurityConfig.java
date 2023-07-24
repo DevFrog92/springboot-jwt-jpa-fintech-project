9648d34dbb98d42549f4bc458f789db8b071eb23
DevFrog92 <yongsu1481@gmail.com>
2023-07-24 13:34:32 +0900

    chore: add security configuration

---
+++ src/main/java/com/fintech/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.fintech.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // todo set authentication and authorization
+        http.authorizeRequests()
+                .antMatchers("/**")
+                .permitAll()
+                .and()
+                .csrf().disable();
+        return http.build();
+    }
+}
