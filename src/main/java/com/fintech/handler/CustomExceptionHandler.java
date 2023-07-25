e45149e8da5d089fbbaa822e35db6f600e76b756
DevFrog92 <yongsu1481@gmail.com>
2023-07-25 17:40:39 +0900

    refactor: add logging in CustomExceptionHandler

--- src/main/java/com/fintech/handler/CustomExceptionHandler.java
+++ src/main/java/com/fintech/handler/CustomExceptionHandler.java
@@ -3,16 +3,19 @@ package com.fintech.handler;
 import com.fintech.dto.ResponseDto;
 import com.fintech.exception.CustomUserApiException;
 import com.fintech.exception.CustomValidationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
     @ExceptionHandler(CustomUserApiException.class)
     public ResponseEntity<?> userApiException(CustomUserApiException e) {
+        log.info("[Exception] User api exception", e);
         return new ResponseEntity<>(
                 new ResponseDto<>(HttpStatus.BAD_REQUEST.value(),
                         e.getMessage(),
@@ -23,6 +26,7 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class)
     public ResponseEntity<?> validationException(CustomValidationException e) {
+        log.info("[Exception] validation exception", e);
         return new ResponseEntity<>(
                 new ResponseDto<>(
                         HttpStatus.BAD_REQUEST.value(),
@@ -35,6 +39,7 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> rootException(Exception e) {
+        log.info("[Exception] Internal server error", e);
         return new ResponseEntity<>(
                 new ResponseDto<>(
                         HttpStatus.BAD_REQUEST.value(),
