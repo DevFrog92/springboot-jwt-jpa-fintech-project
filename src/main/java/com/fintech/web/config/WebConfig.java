690e87b2fddd8db1d31886301f5754bd8df402f3
DevFrog92 <yongsu1481@gmail.com>
2023-08-04 14:45:20 +0900

    feat: implement LoginCheckInterceptor and WebConfig

---
+++ src/main/java/com/fintech/web/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.fintech.web.config;
+
+import com.fintech.web.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/css/**", "/*.ico", "/error", "/", "/api/user/**"
+                );
+    }
+}
